74d7a1ce3d3d2a0af8fbecf476d71384d51fd16b
work-artonest-flutter <anilnavadiyaflutter.artonest@gmail.com>
2023-03-15 13:08:00 +0530

    Initial commit

---
+++ lib/models/movie_model.dart
@@ -0,0 +1,79 @@
+class Movie {
+  Movie({
+    required this.name,
+    required this.image,
+  });
+  final String name;
+  final String image;
+}
+
+final movies = [
+  Movie(
+    name: 'Pirates of the Caribbean',
+    image:
+        'https://i.pinimg.com/236x/23/ff/07/23ff07b3be01046c20fdced300040d9b.jpg',
+  ),
+  Movie(
+    name: 'Home Alone',
+    image: 'https://i.pinimg.com/236x/4c/80/ac/4c80ac0ccf18e1a198bc6b4120a43a79.jpg',
+  ),
+  Movie(
+    name: 'T I M E  I S  T H E  E N E M Y',
+    image: 'https://i.pinimg.com/236x/df/a6/9e/dfa69e9b6b6303b7053f7bd4be3b301c.jpg',
+  ),
+  Movie(
+    name: 'AVENGERS',
+    image: 'https://i.pinimg.com/236x/37/27/b5/3727b547ac21f5f3c6c098abc6a3d3c8.jpg',
+  ),
+  Movie(
+    name: 'Doctor Strange',
+    image: 'https://i.pinimg.com/236x/d8/61/88/d8618820fda2dab8d29c6fe6a89d8206.jpg',
+  ),
+  Movie(
+    name: 'Babylon',
+    image: 'https://i.pinimg.com/564x/fd/52/1a/fd521a7dc974924f3fcdf78d11e5e57c.jpg',
+  ),
+  Movie(
+    name: 'Minions',
+    image: 'https://i.pinimg.com/236x/25/21/d6/2521d66145c91e8e49e20b9649e7d4a7.jpg',
+  ),
+  Movie(
+    name: 'Avatar - The way of water',
+    image: 'https://i.pinimg.com/originals/5d/82/af/5d82afebae0e8e8e8e21b0a48e418c4b.jpg',
+  ),
+  Movie(
+    name: 'Jurassic Park',
+    image: 'https://i.pinimg.com/236x/70/1c/84/701c84f1d33c7de01f9e393481f7b146.jpg',
+  ),
+  Movie(
+    name: 'Black Panther',
+    image: 'https://wallpaperaccess.com/full/96632.jpg',
+  ),
+  Movie(
+    name: 'OBLIVION',
+    image:
+        'https://i.pinimg.com/236x/e1/50/11/e150116dbb0c2516b3ec7a2accbb00fe.jpg',
+  ),
+  Movie(
+    name: 'Sherlock Holmes',
+    image:
+        'https://i.pinimg.com/236x/e2/6a/10/e26a101d6627ef60d0f05cff0e8bcec1.jpg',
+  ),
+  Movie(
+    name: 'Tell it like a woman',
+    image: 'https://i.pinimg.com/236x/de/c6/e1/dec6e13cd40bdeff141f62e3a1c1cf44.jpg',
+  ),
+  Movie(
+    name: 'Jumanji The Next Level',
+    image: 'https://i.pinimg.com/236x/92/c3/60/92c3600cc998595ea9740d1e6ce84212.jpg',
+  ),
+  Movie(
+    name: 'The Batman',
+    image: 'https://i.pinimg.com/236x/81/63/dd/8163ddba5895a3c8a0bd24a3637e40d8.jpg',
+  ),
+  Movie(
+    name: 'The Mummy',
+    image:
+        'https://i.pinimg.com/236x/f5/59/8a/f5598ab3ebbb6068046eba60f74f26e3.jpg',
+  ),
+];
